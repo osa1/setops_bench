5fae64676279932f0e6a52f2c3284f60b775e26d
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-01-07 20:14:45 -0500

    update benchmark program

--- app/Main.hs
+++ app/Main.hs
@@ -1,112 +1,111 @@
 module Main (main) where
 
-import qualified Data.IntSet    as IS
-import           Data.List      (nub, union)
-import qualified Data.Set       as S
+--------------------------------------------------------------------------------
+
+import           Control.Monad
+import qualified Data.IntSet       as IS
+import           Data.List         (intersect, nub, union, (\\))
+import qualified Data.Map          as M
+import qualified Data.Set          as S
 
 import           Criterion.Main
 
 import           Lib
 
+--------------------------------------------------------------------------------
+
 main :: IO ()
-main = defaultMain
-  [ mkSetBench 6, mkIntSetBench 6, mkListSetBench 4,
-    mkBuildSetBench 6, mkBuildIntSetBench 6, mkBuildListSetBench 4,
-    mkBuildAndUnionSetBench 6, mkBuildAndUnionIntSetBench 6,
-    mkListUnionViaSetBench 6, mkListUnionViaIntSetBench 6
-  ]
+main = defaultMain [ unionBench, intersectionBench, lookupBench ]
 
-mkListSetBench, mkSetBench, mkIntSetBench,
-  mkBuildSetBench, mkBuildIntSetBench, mkBuildListSetBench,
-  mkBuildAndUnionSetBench, mkBuildAndUnionIntSetBench,
-  mkListUnionViaSetBench, mkListUnionViaIntSetBench :: Int -> Benchmark
+--------------------------------------------------------------------------------
+-- Benchmarks
 
-mkListSetBench order =
-    bgroup "List set" (map mk_bench [ 0 .. order ])
+unionBench :: Benchmark
+unionBench =
+    bgroup "union" [ union_set, union_intset, union_list,
+                     union_list_via_set, union_list_via_intset ]
   where
-    mk_bench i =
-      env (return (mkListSets i)) $ \ ~(set1, set2) ->
-        bench ("Sets with size 10^" ++ show i)  (nf (union set1) set2)
+    union_set =
+      bgroup "Set" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (mkSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (S.union set1) set2)
 
-mkSetBench order =
-    bgroup "Set" (map mk_bench [ 0 .. order ])
+    union_intset =
+      bgroup "IntSet" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (mkIntSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (IS.union set1) set2)
+
+    union_list =
+      bgroup "List" $ flip map [ 0 .. 4 ] $ \i ->
+        env (return (mkListSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (union set1) set2)
+
+    union_list_via_set =
+      bgroup "List_via_Set" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (mkListSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (S.union (S.fromList set1)) (S.fromList set2))
+
+    union_list_via_intset =
+      bgroup "List_via_IntSet" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (mkListSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (IS.union (IS.fromList set1)) (IS.fromList set2))
+
+intersectionBench :: Benchmark
+intersectionBench =
+    bgroup "intersection" [ inter_set, inter_intset, inter_list,
+                            inter_list_via_set, inter_list_via_intset ]
   where
-    mk_bench i =
-      env (return (mkSets i)) $ \ ~(set1, set2) ->
-        bench ("Sets with size 10^" ++ show i) (nf (S.union set1) set2)
+    inter_set =
+      bgroup "Set" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (mkSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (S.intersection set1) set2)
 
-mkIntSetBench order =
-    bgroup "IntSet" (map mk_bench [ 0 .. order ])
+    inter_intset =
+      bgroup "IntSet" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (mkIntSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (IS.intersection set1) set2)
+
+    inter_list =
+      bgroup "List" $ flip map [ 0 .. 4 ] $ \i ->
+        env (return (mkListSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (intersect set1) set2)
+
+    inter_list_via_set =
+      bgroup "List_via_Set" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (mkListSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (S.intersection (S.fromList set1)) (S.fromList set2))
+
+    inter_list_via_intset =
+      bgroup "List_via_IntSet" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (mkListSets i)) $ \ ~(set1, set2) ->
+          bench (show i) (nf (IS.intersection (IS.fromList set1)) (IS.fromList set2))
+
+lookupBench :: Benchmark
+lookupBench =
+    bgroup "insertion" [ lookup_set, lookup_intset, lookup_list,
+                         lookup_list_via_set, lookup_list_via_intset ]
   where
-    mk_bench i =
-      env (return (mkIntSets i)) $ \ ~(set1, set2) ->
-        bench ("Sets with size 10^" ++ show i) (nf (IS.union set1) set2)
+    lookup_set =
+      bgroup "Set" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (let (set1, _) = mkSets i in (set1, S.size set1))) $ \ ~(set, size) ->
+          bench (show i) (nf (S.member (size `div` 2)) set)
 
-mkBuildSetBench order =
-    bgroup "Set - fromList" (map mk_bench [ 0 .. order ])
-  where
-    mk_bench i =
-      let lst = [ 0 .. (10^i) - 1 ] :: [Int]
-      in env (return lst) $ \lst ->
-           bench ("S.fromList for size 10^" ++ show i) (nf S.fromList lst)
+    lookup_intset =
+      bgroup "IntSet" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (let (set1, _) = mkIntSets i in (set1, IS.size set1))) $ \ ~(set, size) ->
+          bench (show i) (nf (IS.member (size `div` 2)) set)
 
-mkBuildIntSetBench order =
-    bgroup "IntSet - fromList" (map mk_bench [ 0 .. order ])
-  where
-    mk_bench i =
-      env (return [ 0 .. (10^i) - 1 ]) $ \lst ->
-        bench ("IS.fromList for size 10^" ++ show i) (nf IS.fromList lst)
+    lookup_list =
+      bgroup "List" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (let (set1, _) = mkListSets i in (set1, length set1))) $ \ ~(set, size) ->
+          bench (show i) (nf (elem (size `div` 2)) set)
 
-mkBuildListSetBench order =
-    bgroup "List set - nub" (map mk_bench [ 0 .. order ])
-  where
-    mk_bench i =
-      env (return [ 0 :: Int .. (10^i) - 1 ]) $ \lst ->
-        bench ("nub for size 10^" ++ show i) $
-          nf nub lst
+    lookup_list_via_set =
+      bgroup "List_via_Set" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (let (set1, _) = mkListSets i in (set1, length set1))) $ \ ~(set, size) ->
+          bench (show i) (nf (S.member (size `div` 2)) (S.fromList set))
 
-mkBuildAndUnionSetBench order =
-    bgroup "Set - fromSet and union" (map mk_bench [ 0 .. order ])
-  where
-    mk_bench i =
-      bench ("S.fromList for size 10^" ++ show i) $ nf go i
-
-    go i =
-      let
-        size = 10 ^ i
-
-        lst1, lst2 :: [Int]
-        lst1 = [ 0    .. size - 1     ]
-        lst2 = [ size .. 2 * size - 1 ]
-      in
-        S.fromList lst1 `S.union` S.fromList lst2
-
-mkBuildAndUnionIntSetBench order =
-    bgroup "IntSet - fromSet and union" (map mk_bench [ 0 .. order ])
-  where
-    mk_bench i =
-      bench ("IS.fromList for size 10^" ++ show i) $ nf go i
-
-    go i =
-      let
-        size = 10 ^ i
-        lst1 = [ 0    .. size - 1     ]
-        lst2 = [ size .. 2 * size - 1 ]
-      in
-        IS.fromList lst1 `IS.union` IS.fromList lst2
-
-mkListUnionViaSetBench order =
-    bgroup "List union via Set" (map mk_bench [ 0 .. order ])
-  where
-    mk_bench i =
-      env (return (mkListSets i)) $ \ ~(set1, set2) ->
-        bench ("Build Set, take union, return list back, 10^" ++ show i) $
-          nf (\set2 -> S.toList (S.fromList set1 `S.union` S.fromList set2)) set2
-
-mkListUnionViaIntSetBench order =
-    bgroup "List union via IntSet" (map mk_bench [ 0 .. order ])
-  where
-    mk_bench i =
-      env (return (mkListSets i)) $ \ ~(set1, set2) ->
-        bench ("Build IntSet, take union, return list back, 10^" ++ show i) $
-          nf (\set2 -> IS.toList (IS.fromList set1 `IS.union` IS.fromList set2)) set2
+    lookup_list_via_intset =
+      bgroup "List_via_IntSet" $ flip map [ 0 .. 6 ] $ \i ->
+        env (return (let (set1, _) = mkListSets i in (set1, length set1))) $ \ ~(set, size) ->
+          bench (show i) (nf (IS.member (size `div` 2)) (IS.fromList set))
